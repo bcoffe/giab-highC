7eec6f329b342b35d49be294460e13d7e1ae24e0
Hitzbioinfo <jch3w@virginia.edu>
2015-09-21 21:57:20 -0400

    Update multiinter.pl

--- perl/multiinter.pl
+++ perl/multiinter.pl
@@ -1,10 +1,11 @@
 #!/bin/perl
 
-open $in,"<","/mnt/hgfs/VCF/GIAB/GeT-RM/multiinter";
-#open $bed1,">","/mnt/hgfs/VCF/GIAB/GeT-RM/bed_onetest.bed";
-#print $bed1 'track name="Only 1 test coverage" gffTags=on visibility=2\n';
-#open $bed2,">","/mnt/hgfs/VCF/GIAB/GeT-RM/bed_twotest.bed";
-#print $bed2 'track name="< 3 test coverage" gffTags=on visibility=2\n';
+#From terminal: bedtools multiinter -names converted_*.bed -g converted_*.bed > multiinter
+open $in,"<","/dir/multiinter";
+open $bed1,">","/mnt/hgfs/VCF/GIAB/GeT-RM/bed_onetest.bed";
+print $bed1 'track name="Only 1 test coverage" gffTags=on visibility=2\n';
+open $bed2,">","/mnt/hgfs/VCF/GIAB/GeT-RM/bed_twotest.bed";
+print $bed2 'track name="< 3 test coverage" gffTags=on visibility=2\n';
 open $bed3,">","/mnt/hgfs/VCF/GIAB/GeT-RM/bed_3test.bed";
 print $bed3 'track name="> 2 test coverage" gffTags=on visibility=2',"\n";
 while ($row=<$in>){
@@ -20,9 +21,9 @@ while ($row=<$in>){
 			}
 		}
 		if ($hits == 1){
-			#print $bed1 "$chrom\t$start\t$end\t$test\n";
+			print $bed1 "$chrom\t$start\t$end\t$test\n";
 		}elsif ($hits < 3){
-			#$note=join('|',@tests);
+			$note=join('|',@tests);
 			#print $bed2 "$chrom\t$start\t$end\t$note\n";
 		}elsif ($hits > 2){
 			$note=join('|',@tests);
@@ -30,6 +31,6 @@ while ($row=<$in>){
 		}
 	}
 }
-#close $bed1;
-#close $bed2;
+close $bed1;
+close $bed2;
 close $bed3;
